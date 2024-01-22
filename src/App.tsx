939a6ad806acff81be4366c6e282622133b4cefd
Piotr Karaś <elexxx214@interia.pl>
2024-01-22 20:19:27 +0100

    Created website layout(grid)

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,14 @@ import './App.css'
 function App() {
 
   return (
-      <div>Start Our App here</div>
+  
+  <div className="container">
+  <div className="header"><h1>header</h1></div>
+  <div className="nav-left"><h1>nav-left</h1></div>
+  <div className="footer"><h1>footer</h1></div>
+  <div className="content"><h1>content</h1></div>
+  </div>
+      
   )
 }
 
